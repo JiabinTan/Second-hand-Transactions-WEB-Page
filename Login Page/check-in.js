a68541d071117769f7d9eebf97f44c7ccbcf8dc7
KOMOhub <1035844563@qq.com>
2017-07-26 09:31:31 +0800

    登陆界面
    这是登陆界面，但还没有完全完成，后面还需修改

---
+++ Login Page/check-in.js
@@ -0,0 +1,51 @@
+﻿function GetRandomNum(Min,Max) {
+    var Range = Max - Min;
+    var Rand = Math.random();
+    return (Min + Math.round(Rand * Range));
+}
+function AnimationContinue(e,w)
+{
+    $(e).animate({ left: "0px" }, GetRandomNum(2, 6) * 1000).animate({ left: String(window.innerWidth) + "px" }, GetRandomNum(2, 6) * 1000, 'swing', function ()
+    {
+        AnimationContinue(this,w);
+    });
+}
+$(document).ready(
+    function () {
+        var Rcolor = new Array("red", "green", "blue", "black", "orange", "#c7c7c7", "#7a378b", "yellow");
+        var wid = new Array();
+        var Wmax = window.innerWidth;
+        for (var i = 0; i <= 7; i++) {
+            var a = String(i);
+            wid[i] = GetRandomNum(200, Wmax);
+            $(".color" + a).css(
+                {
+                    "height": String(window.innerHeight / 20) + "px",
+                    "width": String(wid[i]) + "px",
+                    "background-color": Rcolor[i],
+                    "opacity": .6,
+                    "left": String(-wid[i]) + "px"
+                }
+                );
+           
+            $(".color" + a).delay(GetRandomNum(0, 1) * 1000).animate({ left: String(window.innerWidth) + "px" }, GetRandomNum(2, 6) * 1000, 'swing', function ()
+            {
+                AnimationContinue(this,-wid[i]);
+            });
+        }
+        
+        $("#log-info").css(
+            {
+                "left": String(window.innerWidth / 2 - 420) + "px",
+                "top":String(window.innerHeight/2-260)+"px"
+            }
+            );
+        $("#remLB").click(
+            function ()
+            {
+                if ($("#remmember").prop("checked"))
+                    $("#remmember").prop("checked", false);
+                else
+                    $("#remmember").prop("checked", true);
+            });
+    });
